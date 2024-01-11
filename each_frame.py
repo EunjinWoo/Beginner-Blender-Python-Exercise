e12089c274e16b9410fc687e868bc10d74e9d1f0
EunjinWoo <92513850+EunjinWoo@users.noreply.github.com>
2024-01-11 21:21:46 +0900

    Update and rename frame0.py to each_frame.py

--- each_frame.py
+++ each_frame.py
@@ -5,7 +5,7 @@ import bpy
 import numpy as np
 
 # extend Python's math functionality
-#import math
+import math
 
 # targets
 targets = ["mixamorig9:LeftUpLeg", 
@@ -55,13 +55,11 @@ euler_frame0 = [
 #                np.array([0.,0.,0.]),
                 
                 # LeftShoulder
-#                [-0.01594712, 0.00780151, -0.90994567], 
-                [-0.01594712, 0.00780151, 0.90994567], 
+                [-0.01594712, 0.00780151, -0.90994567], 
 #                np.array([0.,0.,0.]),
                 
                 # LeftForeArm
-#                [ 0.93867956, -0.08973764, -0.17660835], 
-                [ 0.93867956, -0.08973764, 0.17660835], 
+                [ 0.93867956, -0.08973764, -0.17660835], 
 #                np.array([0.,0.,0.]),
                 
                 # LeftHand
@@ -69,13 +67,11 @@ euler_frame0 = [
 #                np.array([0.,0.,0.]),
                 
                 # RightShoulder
-#                [0.0533663 , 0.02459438, 0.863554 ], 
-                [0.0533663 , 0.02459438, -0.863554 ], 
+                [0.0533663 , 0.02459438, 0.863554 ],
 #                np.array([0.,0.,0.]),
                 
                 # RightForeArm
-#                [-0.73090292, -0.14607527, 0.37787561], 
-                [-0.73090292, -0.14607527, -0.37787561], 
+                [-0.73090292, -0.14607527, 0.37787561],
 #                np.array([0.,0.,0.]),
                 
                 # RightHand
@@ -87,7 +83,7 @@ euler_frame0 = [
 #                np.array([0.,0.,0.]),
                 
                 # Neck
-                [5.00233233e-04, 2.20672709e-02, 3.09626514e+00],
+                np.array([5.00233233e-04, 2.20672709e-02, 3.09626514e+00]),
 #                np.array([0.,0.,0.]),
                 
                 ]
@@ -126,6 +122,60 @@ euler_frame0 = [
 #                np.array([0.,0.,0.]),
 #                ]
 
+# Decompose ZXY
+#euler_frame300 = [
+#                np.array([ 0.62597033,  0.72275924, -0.24343524]), 
+#                np.array([-0.32280454, -0.22570775, -0.03690204]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.06397794,  0.60802306,  0.02007843]), 
+#                np.array([ 0.08519165, -0.02473634,  0.00105436]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.23429985, -0.01458588,  0.12377988]), 
+#                np.array([-0.25497261,  0.01959656, -0.15258947]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([ 1.51105682, -0.39924061, -0.42312582]), 
+#                np.array([1.33255188, 0.21414169, 0.27173313]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.23631965,  0.04637208, -0.21550363]), 
+#                np.array([-0.8561505 ,  1.25292613, -2.01651646]),
+#                ]
+
+# Decompose ZYX
+euler_frame300 = [
+                np.array([ 0.32018722,  0.68415048, -0.11486783]), 
+                np.array([-0.25548312, -0.20419534, -0.02631704]), 
+                np.array([0., 0., 0.]), 
+                np.array([-0.21402233,  0.56639147,  0.06250273]), 
+                np.array([ 0.04928659, -0.03057812,  0.00075376]), 
+                np.array([0., 0., 0.]), 
+                np.array([-0.15513398, -0.03059995,  0.38876468]), 
+                np.array([ 0.06076612, -0.00903323, -0.29506237]), 
+                np.array([0., 0., 0.]), 
+                np.array([2.17589188, 0.94828593, 0.52562199]), 
+                np.array([0.77629487, 0.27334028, 0.64891303]), 
+                np.array([0., 0., 0.]), 
+                np.array([-0.03017312,  0.04746849, -0.21526832]), 
+                np.array([-3.10736546,  2.90196898, -0.28244139]),
+                ]
+                
+# Decompose XYZ (ChatGPT)
+#euler_frame300 = [
+#                np.array([0.73255337, 0.18135932, 0.46550754]), 
+#                np.array([-0.22184923,  0.03565699, -0.31747326]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([ 0.60960953, -0.01657267, -0.05266613]), 
+#                np.array([-0.02572539, -0.00109238,  0.08487076]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([ 0.01481046,  0.12507051, -0.23543574]), 
+#                np.array([-0.01970438, -0.15113364, -0.25881184]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([ 0.43963495, -0.39352699,  1.68461238]), 
+#                np.array([-0.21748485,  0.25988217,  1.3919602 ]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.00476714, -0.2108183 , -0.23487474]), 
+#                np.array([-1.70763234, -0.27643974, -2.90024129])
+#                ]
+
 # get a reference to the currently active object
 armature = bpy.context.active_object # bpy.context.active_object / bpy.data.objects['Armature']
 
@@ -133,7 +183,11 @@ for bone_index, bone_name in enumerate(targets):
     # set each bone's rotation mode to 'ZXY'(euler) from 'QUATERNION'
     armature.pose.bones[bone_name].rotation_mode = 'ZXY'
 #    
-    for i, angle in enumerate(euler_frame0[bone_index]):
-        print(bone_name + str(i) + " " + str(angle))
-        armature.pose.bones[bone_name].rotation_euler[i] = angle
+    for i, angle in enumerate(euler_frame300[bone_index]):
+        print(f"{bone_name}{i} {angle:.4e}")
+        # put each bone_angle array into ZXY angles of this character's rotation angle
+        armature.pose.bones[bone_name].rotation_euler[(i+2)%3] = angle
 #            armature.pose.bones[bone_name].rotation_euler = euler_angles
+        
+#    if bone_name == 'mixamorig9:Neck':
+#        armature.pose.bones[bone_name].rotation_euler.y += math.radians(180)
