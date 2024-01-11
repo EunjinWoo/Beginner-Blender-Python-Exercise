c51740946e89f02f963f4deb2517d0de3a93e4b2
EunjinWoo <92513850+EunjinWoo@users.noreply.github.com>
2024-01-11 21:21:53 +0900

    Update each_frame.py

--- each_frame.py
+++ each_frame.py
@@ -28,6 +28,7 @@ targets = ["mixamorig9:LeftUpLeg",
            "mixamorig9:Neck",
            ]
 
+
 # eulerangles frame 0
 euler_frame0 = [
                 # LeftUpLeg
@@ -123,40 +124,40 @@ euler_frame0 = [
 #                ]
 
 # Decompose ZXY
-#euler_frame300 = [
-#                np.array([ 0.62597033,  0.72275924, -0.24343524]), 
-#                np.array([-0.32280454, -0.22570775, -0.03690204]), 
-#                np.array([0., 0., 0.]), 
-#                np.array([-0.06397794,  0.60802306,  0.02007843]), 
-#                np.array([ 0.08519165, -0.02473634,  0.00105436]), 
-#                np.array([0., 0., 0.]), 
-#                np.array([-0.23429985, -0.01458588,  0.12377988]), 
-#                np.array([-0.25497261,  0.01959656, -0.15258947]), 
-#                np.array([0., 0., 0.]), 
-#                np.array([ 1.51105682, -0.39924061, -0.42312582]), 
-#                np.array([1.33255188, 0.21414169, 0.27173313]), 
-#                np.array([0., 0., 0.]), 
-#                np.array([-0.23631965,  0.04637208, -0.21550363]), 
-#                np.array([-0.8561505 ,  1.25292613, -2.01651646]),
-#                ]
+euler_frame300 = [
+                np.array([ 0.62597033,  0.72275924, -0.24343524]), 
+                np.array([-0.32280454, -0.22570775, -0.03690204]), 
+                np.array([0., 0., 0.]), 
+                np.array([-0.06397794,  0.60802306,  0.02007843]), 
+                np.array([ 0.08519165, -0.02473634,  0.00105436]), 
+                np.array([0., 0., 0.]), 
+                np.array([-0.23429985, -0.01458588,  0.12377988]), 
+                np.array([-0.25497261,  0.01959656, -0.15258947]), 
+                np.array([0., 0., 0.]), 
+                np.array([ 1.51105682, -0.39924061, -0.42312582]), 
+                np.array([1.33255188, 0.21414169, 0.27173313]), 
+                np.array([0., 0., 0.]), 
+                np.array([-0.23631965,  0.04637208, -0.21550363]), 
+                np.array([-0.8561505 ,  1.25292613, -2.01651646]),
+                ]
 
 # Decompose ZYX
-euler_frame300 = [
-                np.array([ 0.32018722,  0.68415048, -0.11486783]), 
-                np.array([-0.25548312, -0.20419534, -0.02631704]), 
-                np.array([0., 0., 0.]), 
-                np.array([-0.21402233,  0.56639147,  0.06250273]), 
-                np.array([ 0.04928659, -0.03057812,  0.00075376]), 
-                np.array([0., 0., 0.]), 
-                np.array([-0.15513398, -0.03059995,  0.38876468]), 
-                np.array([ 0.06076612, -0.00903323, -0.29506237]), 
-                np.array([0., 0., 0.]), 
-                np.array([2.17589188, 0.94828593, 0.52562199]), 
-                np.array([0.77629487, 0.27334028, 0.64891303]), 
-                np.array([0., 0., 0.]), 
-                np.array([-0.03017312,  0.04746849, -0.21526832]), 
-                np.array([-3.10736546,  2.90196898, -0.28244139]),
-                ]
+#euler_frame300 = [
+#                np.array([ 0.32018722,  0.68415048, -0.11486783]), 
+#                np.array([-0.25548312, -0.20419534, -0.02631704]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.21402233,  0.56639147,  0.06250273]), 
+#                np.array([ 0.04928659, -0.03057812,  0.00075376]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.15513398, -0.03059995,  0.38876468]), 
+#                np.array([ 0.06076612, -0.00903323, -0.29506237]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([2.17589188, 0.94828593, 0.52562199]), 
+#                np.array([0.77629487, 0.27334028, 0.64891303]), 
+#                np.array([0., 0., 0.]), 
+#                np.array([-0.03017312,  0.04746849, -0.21526832]), 
+#                np.array([-3.10736546,  2.90196898, -0.28244139]),
+#                ]
                 
 # Decompose XYZ (ChatGPT)
 #euler_frame300 = [
@@ -176,18 +177,84 @@ euler_frame300 = [
 #                np.array([-1.70763234, -0.27643974, -2.90024129])
 #                ]
 
+
 # get a reference to the currently active object
 armature = bpy.context.active_object # bpy.context.active_object / bpy.data.objects['Armature']
 
+keypoints = np.load("/Users/esthrelar/Documents/Programming/23FW_UnivProject/joint_angles_calculate/euler.npy")
+print(f"len(keypoints) : {len(keypoints)}")
+
+
+# set each bone's rotation mode to 'ZXY'(euler) from 'QUATERNION'
 for bone_index, bone_name in enumerate(targets):
-    # set each bone's rotation mode to 'ZXY'(euler) from 'QUATERNION'
     armature.pose.bones[bone_name].rotation_mode = 'ZXY'
+#    armature.pose.bones[bone_name].rotation_mode = 'XYZ'
+#    armature.pose.bones[bone_name].rotation_mode = 'YXZ'
 #    
-    for i, angle in enumerate(euler_frame300[bone_index]):
-        print(f"{bone_name}{i} {angle:.4e}")
-        # put each bone_angle array into ZXY angles of this character's rotation angle
-        armature.pose.bones[bone_name].rotation_euler[(i+2)%3] = angle
-#            armature.pose.bones[bone_name].rotation_euler = euler_angles
+#    for i, angle in enumerate(euler_frame300[bone_index]):
+#        # put each bone_angle array into ZXY angles of this character's rotation angle
+#        if i == 0:
+#            armature.pose.bones[bone_name].rotation_euler.z = angle
+#        elif i == 1:
+#            armature.pose.bones[bone_name].rotation_euler.x = angle
+#        elif i == 2:
+#            armature.pose.bones[bone_name].rotation_euler.y = angle
+#        
+#    if bone_name == 'mixamorig9:Hips':
+#        armature.pose.bones[bone_name].rotation_euler.z += math.radians(0)
+#        armature.pose.bones[bone_name].rotation_euler.y += math.radians(0)
+#        armature.pose.bones[bone_name].rotation_euler.x += math.radians(0)
+
+#    if bone_name == 'mixamorig9:RightShoulder':
+#    #        armature.pose.bones[bone_name].rotation_euler.z += math.radians(270)
+#            armature.pose.bones[bone_name].rotation_euler.y += math.radians(-0)
+#    #        armature.pose.bones[bone_name].rotation_euler.x += math.radians(90)
+    
+for framenum in range(1,len(keypoints)+1): # 1 ~ 531
+    for frame_keypoints, bone_name in zip(keypoints[framenum-1], targets):
+        print(f"keypoints[i] = {frame_keypoints}, targets = {bone_name}")
         
-#    if bone_name == 'mixamorig9:Neck':
-#        armature.pose.bones[bone_name].rotation_euler.y += math.radians(180)
+        for i, angle in enumerate(frame_keypoints):
+            # put each bone_angle array into ZXY angles of this character's rotation angle
+            if bone_name == 'mixamorig9:Neck':
+                if i == 0:
+                    armature.pose.bones[bone_name].rotation_euler.z = 0
+                elif i == 1:
+                    armature.pose.bones[bone_name].rotation_euler.x = 0
+                elif i == 2:
+                    armature.pose.bones[bone_name].rotation_euler.y = 0
+                    
+            elif bone_name == 'mixamorig9:LeftShoulder' or bone_name == 'mixamorig9:RightShoulder' or bone_name == 'mixamorig9:LeftForeArm' or bone_name == 'mixamorig9:RightForeArm':
+                if i == 0:
+                    armature.pose.bones[bone_name].rotation_euler.z = (-1) * angle
+                elif i == 1:
+                    armature.pose.bones[bone_name].rotation_euler.x = (-1) * angle
+                elif i == 2:
+                    armature.pose.bones[bone_name].rotation_euler.y = (-1) * angle
+                    
+            else:
+                if i == 0:
+                    armature.pose.bones[bone_name].rotation_euler.z = angle
+                elif i == 1:
+                    armature.pose.bones[bone_name].rotation_euler.x = angle
+                elif i == 2:
+                    armature.pose.bones[bone_name].rotation_euler.y = angle
+                
+        
+                
+        # insert keyframe
+        armature.pose.bones[bone_name].keyframe_insert('rotation_euler',frame=framenum)
+        
+# ----------------------------------------------------------
+# 특정 bone의 이름 지정
+bone_name = "mixamorig9:RightShoulder"
+
+# 특정 bone 가져오기
+bone = armature.pose.bones.get(bone_name)
+
+if bone:
+    # 로컬 좌표계에서의 오일러 각도 출력
+    local_rotation_euler = bone.rotation_euler
+    print("Local Euler Rotation (degrees):", [math.degrees(angle) for angle in local_rotation_euler])
+else:
+    print(f"Bone '{bone_name}' not found.")
